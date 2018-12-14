89bbddb58b3069d4b8d40113911ad465b9f2bb57
Trond Bergquist <trond_bergquist@hotmail.com>
2018-12-14 20:08:10 +0100

    Add flow & lint

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,14 @@
-module.exports = function(api) {
+// @flow
+
+/*::
+ type API = {|
+  +cache: boolean => void,
+|};
+*/
+
+module.exports = function(api /* :API */) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo', 'flow'],
   };
 };
