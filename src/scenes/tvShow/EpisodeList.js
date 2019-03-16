f8aff386a48bef797d7c2b892a853624a94a9d60
Trond Bergquist <trond_bergquist@hotmail.com>
2019-03-16 15:36:50 +0100

    Start with episode list open
    Add animation for icon

--- src/scenes/tvShow/EpisodeList.js
+++ src/scenes/tvShow/EpisodeList.js
@@ -3,7 +3,7 @@
 import * as React from 'react';
 import { graphql, createFragmentContainer } from '@tbergq/tvhelper-relay';
 import { Text, Touchable, Colors } from '@tbergq/tvhelper-components';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Animated } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
 import type { EpisodeList as EpisodeListType } from './__generated__/EpisodeList.graphql';
@@ -18,21 +18,43 @@ type State = {|
 |};
 
 class EpisodeList extends React.Component<Props, State> {
-  state = {
-    showEpisodes: false,
-  };
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      showEpisodes: true,
+    };
+
+    this.iconAnimation = new Animated.Value(0);
+  }
+
+  iconAnimation: Animated.Value;
 
   toggleEpisodes = () => {
     setTimeout(() => {
-      this.setState(state => ({
-        showEpisodes: !state.showEpisodes,
-      }));
+      this.setState(
+        state => ({
+          showEpisodes: !state.showEpisodes,
+        }),
+        this.animate,
+      );
     });
+  };
+
+  animate = () => {
+    Animated.timing(this.iconAnimation, {
+      toValue: this.state.showEpisodes ? 0 : 1,
+      duration: 250,
+      useNativeDriver: true,
+    }).start();
   };
 
   render() {
     const data = this.props.data.episodes ?? [];
-
+    const transformation = this.iconAnimation.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '180deg'],
+    });
     return (
       <>
         <Touchable
@@ -43,11 +65,13 @@ class EpisodeList extends React.Component<Props, State> {
           ]}
         >
           <Text bold={true}>Episodes</Text>
-          <MaterialIcons
-            name={this.state.showEpisodes ? 'expand-less' : 'expand-more'}
-            size={20}
-            color={Colors.primary}
-          />
+          <Animated.View style={{ transform: [{ rotate: transformation }] }}>
+            <MaterialIcons
+              name={'expand-less'}
+              size={20}
+              color={Colors.primary}
+            />
+          </Animated.View>
         </Touchable>
         {this.state.showEpisodes &&
           data.map(episode => {
